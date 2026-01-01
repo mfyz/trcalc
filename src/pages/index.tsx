7e40fea75d7b13fc6843d1fad104237d55333646
Fatih Yildiz <fatih@mfyz.com>
2025-12-31 19:43:36 -0500

    auto-reset icon enabled disabled state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -83,7 +83,7 @@ export default function Home() {
                 <svg
                   id="auto-reset-icon"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-300"
+                  className={`h-5 w-5 transition-opacity ${settings.autoReset ? 'text-yellow-500' : 'text-gray-400 opacity-40'}`}
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
